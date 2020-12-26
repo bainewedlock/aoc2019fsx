29e8a5a33e156275f5501e7a550676e91e9d73be
Baine Wedlock <bainewedlock@gmail.com>
2020-12-26 22:36:39 +0100

    day 6

--- common/AocLib.fsx
+++ common/AocLib.fsx
@@ -8,3 +8,15 @@ let trimStr (input:string) = input.Trim()
 let toTuple = function
     | [a;b] -> a, b
     | x     -> failwithf "unexpected: %A" x
+
+let bfs fAdjacentEdges (start:'a) = seq {
+    let mutable queue = [start,[]]
+    let discovered = System.Collections.Generic.HashSet<'a>([start])
+    while queue <> [] do
+        let (v,log)::rest = queue
+        queue <- rest
+        if log <> [] then yield log
+        for edge in fAdjacentEdges v do
+            if not (discovered.Contains edge) then
+                discovered.Add edge |> ignore
+                queue <- (edge,edge::log)::queue }
--- day06/script.fsx
+++ day06/script.fsx
@@ -1,35 +1,83 @@
 open System.IO
-open System.Text.RegularExpressions
 
 #load "../common/AocLib.fsx"
 open AocLib
 
-let demoinput = "COM)B
-                 B)C
-                 C)D
-                 D)E
-                 E)F
-                 B)G
-                 G)H
-                 D)I
-                 E)J
-                 J)K
-                 K)L"
+type Tree = Map<string,string Set>
 
-let parse =
+let demoinput = "COM)B
+                   B)C
+                   C)D
+                   D)E
+                   E)F
+                   B)G
+                   G)H
+                   D)I
+                   E)J
+                   J)K
+                   K)L"
+
+let parse : string -> Tree =
     split '\n'
     >> List.map (trimStr >> split ')' >> toTuple)
-    >> List.map (fun (a,b) -> b,a)
+    >> List.map (fun (a,b) -> b, Set [a])
     >> Map
-parse demoinput
+let demotree = parse demoinput
 
-let orbitsOf (t:Map<string, string>) =
-    List.unfold (t.TryFind >> Option.map (fun o' -> o', o'))
+let orbitsOf (t:Tree) =
+    List.unfold (
+        t.TryFind
+        >> Option.map (Seq.exactlyOne >> fun o -> o, o))
 
-let solve input =
+let solvePart1 input =
     let t = parse input
     t
     |> Map.toList
     |> List.collect (fst >> orbitsOf t)
     |> List.length
-solve demoinput
+solvePart1 demoinput
+
+
+let input = File.ReadAllText "day06/input.txt"
+solvePart1 input
+
+
+let reverse (tree:Tree) : Tree =
+    tree
+    |> Map.toList
+    |> List.map (fun (a,b) -> b,a)
+    |> List.groupBy fst
+    |> List.map (fun (k, vs) -> k |> Seq.exactlyOne, vs |> List.map snd |> Set)
+    |> Map
+reverse demotree
+
+let demoinput2 = "COM)B
+                    B)C
+                    C)D
+                    D)E
+                    E)F
+                    B)G
+                    G)H
+                    D)I
+                    E)J
+                    J)K
+                    K)L
+                    K)YOU
+                    I)SAN"
+let demotree2 = parse demoinput2
+
+let adjacentEdges (tree:Tree) =
+    let tree':Tree = reverse tree
+    fun edge ->
+        [ tree; tree' ]
+        |> List.choose (Map.tryFind edge >> Option.map Set.toList)
+        |> List.concat
+adjacentEdges demotree2 "D"
+
+let solvePart2 input =
+    let tree = parse input
+    bfs (adjacentEdges tree) "YOU"
+    |> Seq.find (List.head >> ((=)"SAN"))
+    |> Seq.length
+    |> fun l -> l - 2
+solvePart2 input
